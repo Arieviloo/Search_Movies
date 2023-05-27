f31f56432eac382d8dbf94abeb5c94c0e71fc157
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-05-27 11:19:22 -0300

    Adding extension func pin

---
+++ SearchMovies/Extension/UIView + Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView + Extension.swift
+//  SearchMovies
+//
+//  Created by Jadiê on 27/05/23.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    public func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor)
+        ])
+    }
+}
