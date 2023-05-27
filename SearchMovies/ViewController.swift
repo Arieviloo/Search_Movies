559ca4179e81a0ad540a3cd4158e13d4850e3af0
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-05-27 11:05:30 -0300

    Init

---
+++ SearchMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Jadiê on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
